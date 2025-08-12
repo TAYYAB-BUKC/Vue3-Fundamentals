812423e970bff10e553642b68d2ab956f160a921
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-12 07:47:19 +0500

    ButtonCounter component registration implemented.

--- Components/vue-components-demo/src/main.js
+++ Components/vue-components-demo/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import Application from './components/App.vue'
 import ContactUs from './components/ContactUs.vue';
+import ButtonCounter from './components/ButtonCounter.vue';
 
 const app = createApp(Application)
 
 app.component('contact-us', ContactUs);
+app.component('button-counter', ButtonCounter);
 
 app.mount('#app');
