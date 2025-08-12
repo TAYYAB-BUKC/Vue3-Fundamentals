b5a033cc3aafc6757bb33099c43d97e0d0b39ab8
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-12 07:46:11 +0500

    ButtonCounter component implemented.

---
+++ Components/vue-components-demo/src/components/ButtonCounter.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>Counter: {{ counter }}</p>
+    <button type="button" name="BtnIncrementCounter" @click="counter++"> Button clicked {{ counter }} times.</button>
+</template>
+
+<script>
+    import { ref } from "vue";
+    export default {
+        setup(){
+            const counter = ref(0);
+
+            return {
+                counter
+            }
+        }
+    }
+</script>
