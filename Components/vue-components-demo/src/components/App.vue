d7111c515ae96b82bec1c4b09cf39db6dfe63b97
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-12 09:27:36 +0500

    Dynamic props implemented between the parent (App) and the child (ContactUs) component.

--- Components/vue-components-demo/src/components/App.vue
+++ Components/vue-components-demo/src/components/App.vue
@@ -6,6 +6,9 @@
         <div>
             {{message}}
         </div>
+        <div>
+            <input type="text" v-model="ownerName" />
+        </div>
         <div class="row">
             <div class="col-12" v-for="contact in contactList" :key="contact.id">
                 <contact-us :contact-name=contact.name :contact-phone=contact.phone :contact-email=contact.email
@@ -17,32 +20,32 @@
 </template>
 
 <script setup>
-    import { reactive } from 'vue';
+    import { ref, reactive } from 'vue';
     import ContactUs from './ContactUs.vue';
     import ButtonCounter from './ButtonCounter.vue';
 
     const message = 'Hello World From Vue 3 + VITE';
-    const ownerName = 'Tayyab Arsalan';
+    const ownerName = ref('Tayyab Arsalan');
 
     const contactList = reactive([
         {
             id: 1,
             name: 'Tayyab Arsalan',
-            phone: '3409292923',
+            phone: 3409292923,
             email: 'write2tayyabarsalan+vue@gmail.com',
             owner: ownerName
         },
         {
             id: 2,
             name: 'Noman Khan',
-            phone: '3009461798',
+            phone: 3009461798,
             email: 'noman.khan@gmail.com',
             owner: ownerName
         },
         {
             id: 3,
             name: 'Tahir Jaffar',
-            phone: '3103549546',
+            phone: 3103549546,
             email: 'tahir.jaffar@gmail.com',
             owner: ownerName
         }    
