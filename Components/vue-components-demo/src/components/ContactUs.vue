db113220f6a202097b8f070ebe6c80df0d7fdac4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-12 07:54:39 +0500

    Contact email is now dynamic.

--- Components/vue-components-demo/src/components/ContactUs.vue
+++ Components/vue-components-demo/src/components/ContactUs.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>For Contact</h1>
-        <p>Please send an email to write2tayyabarsalan+vue@gmail.com</p>
+        <p>Please send an email to {{ contactEmail }}</p>
     </div>
 </template>
+
+<script setup>
+    import { ref } from "vue";
+    const contactEmail = ref('write2tayyabarsalan+vue@gmail.com');
+</script>
