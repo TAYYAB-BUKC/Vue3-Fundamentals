043d6a2e9ce702f0a58e3d405945b4cac8c79d50
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-12 07:58:11 +0500

    ButtonCounter component global registration removed.

--- Components/vue-components-demo/src/main.js
+++ Components/vue-components-demo/src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
 import Application from './components/App.vue'
 import ContactUs from './components/ContactUs.vue';
-import ButtonCounter from './components/ButtonCounter.vue';
 
 const app = createApp(Application)
 
+// Global Components Registration
 app.component('contact-us', ContactUs);
-app.component('button-counter', ButtonCounter);
 
 app.mount('#app');
