d1b74c120a8fe302d6d7dca0a6b4f3cc39ba5cf2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-13 16:14:15 +0500

    LuckNumber v2 component  implemented.

---
+++ Components/vue-components-demo/src/components/LuckyNumberv2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-6 bg-black text-white text-center p-4">
+        <h3 class="text-success">Random Lucky Number</h3>
+        <hr>
+        <p>
+            Max. Lucky Number <br><input type="text" name="maxNumber" v-model="maxNumber" />
+        </p>
+        <button class="btn btn-outline-primary p-2 mb-3" type="button" @click="generateLuckyNumber()">
+            <span v-if="luckyNumber === 0">Generate Luck Number <br> Range: 1 - {{ maxNumber }}</span>
+            <span v-else>{{ luckyNumber }} <br> Click to generate new lucky number <br> Range: 1 - {{ maxNumber }}</span>
+        </button>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    let luckyNumber = ref(0);
+
+    const maxNumber = ref(1000);
+
+    function generateLuckyNumber(){
+        luckyNumber.value = Math.floor(Math.random() * maxNumber.value);
+    }
+</script>
