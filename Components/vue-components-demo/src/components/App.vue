3d7f06d7519b2ec472fb4725047bcf070214ac9a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-13 16:48:27 +0500

    Include and Exclude of KeepAlive implemented.

--- Components/vue-components-demo/src/components/App.vue
+++ Components/vue-components-demo/src/components/App.vue
@@ -19,7 +19,7 @@
             <button class="btn btn-primary m-2" @click="newVersion = false">Lucky Number v1</button>
             <button class="btn btn-primary m-2" @click="newVersion = true">Lucky Number v2</button>
             <br>
-            <keep-alive>
+            <keep-alive :exclude="['LuckyNumber']" :include="['LuckyNumber', 'LuckyNumberv2']">
                 <component :is="luckyNumbercomponent"></component>
             </keep-alive>
         </div>
