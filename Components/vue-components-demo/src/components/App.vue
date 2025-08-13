2706b14d9f8e0be72d44db5507e31e699f7c3e5e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-13 16:53:25 +0500

    LuckyNumberParent implemented and render in App component.

--- Components/vue-components-demo/src/components/App.vue
+++ Components/vue-components-demo/src/components/App.vue
@@ -13,15 +13,8 @@
             Max. LuckyNumber: <input type="text" v-model="maximumLuckyNumber" />
         </div>
 
-        <div class="text-center">
-            <button class="btn btn-primary m-2" @click="newVersion = !newVersion">Toggle Components</button>
-            <br>
-            <button class="btn btn-primary m-2" @click="newVersion = false">Lucky Number v1</button>
-            <button class="btn btn-primary m-2" @click="newVersion = true">Lucky Number v2</button>
-            <br>
-            <keep-alive :exclude="['LuckyNumber']" :include="['LuckyNumber', 'LuckyNumberv2']">
-                <component :is="luckyNumbercomponent"></component>
-            </keep-alive>
+        <div>
+            <lucky-number-parent></lucky-number-parent>
         </div>
 
         <div class="row">
@@ -51,6 +44,7 @@
     import AddContact from './AddContact.vue';
     import LuckyNumber from './LuckyNumber.vue';
     import LuckyNumberv2 from './LuckyNumberv2.vue';
+    import LuckyNumberParent from './LuckyNumberParent.vue';
 
     const message = 'Hello World From Vue 3 + VITE';
     const ownerName = ref('Tayyab Arsalan');
---
+++ Components/vue-components-demo/src/components/LuckyNumberParent.vue
@@ -0,0 +1,29 @@
+<style>
+</style>
+
+<template>
+    <div class="container">
+        <div class="text-center">
+            <button class="btn btn-primary m-2" @click="newVersion = !newVersion">Toggle Components</button>
+            <br>
+            <button class="btn btn-primary m-2" @click="newVersion = false">Lucky Number v1</button>
+            <button class="btn btn-primary m-2" @click="newVersion = true">Lucky Number v2</button>
+            <br>
+            <keep-alive :exclude="['LuckyNumber']" :include="['LuckyNumber', 'LuckyNumberv2']">
+                <component :is="luckyNumbercomponent"></component>
+            </keep-alive>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref, computed } from 'vue';
+    import LuckyNumber from './LuckyNumber.vue';
+    import LuckyNumberv2 from './LuckyNumberv2.vue';
+
+    const newVersion = ref(false);
+
+    const luckyNumbercomponent = computed(() => {
+        return newVersion.value ? LuckyNumberv2 : LuckyNumber;
+    });
+</script>
