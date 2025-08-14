8c04294aca061a58fb1ac8c7e7f27b58c4acf7e4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 01:59:26 +0500

    Captured and display product id in product detail component.

--- routers/vue-router/src/components/product/detail.vue
+++ routers/vue-router/src/components/product/detail.vue
@@ -1,3 +1,10 @@
 <template>
     <h1>Product Detail</h1>
+    <h1>Product ID: {{ route.params.productId }}</h1>
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+console.log(route);
+</script>
