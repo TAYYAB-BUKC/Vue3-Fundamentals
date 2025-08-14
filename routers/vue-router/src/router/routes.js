3d8f6d27ab52b0fd8ea48b0fbb7eb198c7e356c3
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 01:53:27 +0500

    Product routes configured in routes.js.

--- routers/vue-router/src/router/routes.js
+++ routers/vue-router/src/router/routes.js
@@ -1,13 +1,19 @@
 import {createRouter, createWebHistory} from 'vue-router'; 
 import home from '@/components/home/home.vue';
 import contact from '@/components/home/contact.vue';
+import productList from '@/components/product/list.vue'
+import productDetail from '@/components/product/detail.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', component: home },
+        { path: '/home', component: home },
         { path: '/contact', component: contact },
-        { path: '/contactus', component: contact }
+        { path: '/contactus', component: contact },
+        { path: '/products', component: productList },
+        { path: '/product/list', component: productList },
+        { path: '/product/details/:productId', component: productDetail },
     ]
 });
 
