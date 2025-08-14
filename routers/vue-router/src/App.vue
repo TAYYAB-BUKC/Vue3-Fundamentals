832345a9b1c12f906c863391b24b5754831daad9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 00:32:58 +0500

    Header template added and rendered in App component.

--- routers/vue-router/src/App.vue
+++ routers/vue-router/src/App.vue
@@ -2,8 +2,10 @@
 </style>
 
 <template>
+  <Header></Header>
   <router-view></router-view>
 </template>
 
 <script setup>
+import Header from './components/layout/header.vue';
 </script>
--- routers/vue-router/src/components/layout/header.vue
+++ routers/vue-router/src/components/layout/header.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Header</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Vue Router</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Product
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">List</a></li>
+                <li><a class="dropdown-item" href="#">Details</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <a class="nav-link" href="#">Login</a>
+          </form>
+        </div>
+      </div>
+    </nav>
 </template>
