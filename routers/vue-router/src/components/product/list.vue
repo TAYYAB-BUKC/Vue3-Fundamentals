d90315a024297fba374f31cf8b238fed1df93aa7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 00:00:13 +0500

    Product list component intial template added.

---
+++ routers/vue-router/src/components/product/list.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Product List</h1>
+    <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+        <li>Product 5</li> 
+    </ul>
+</template>
