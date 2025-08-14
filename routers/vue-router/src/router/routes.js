eb145af93831f1ce6716c77e601e3efa65f244cf
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 00:15:08 +0500

    Routes configured in route.js

--- routers/vue-router/src/router/routes.js
+++ routers/vue-router/src/router/routes.js
@@ -1,10 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router'; 
+import home from '@/components/home/home.vue';
+import contact from '@/components/home/contact.vue';
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [{
-
-    }]
+    routes: [
+        { path: '/', component: home },
+        { path: '/contact', component: contact },
+        { path: '/contactus', component: contact }
+    ]
 });
 
 export default router;
