021180410daac52f420c744a1aacbf5a350b598e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 00:37:26 +0500

    Router links added to menu in header component.

--- routers/vue-router/src/components/layout/header.vue
+++ routers/vue-router/src/components/layout/header.vue
@@ -1,30 +1,30 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/">Vue Router</a>
+        <router-link class="navbar-brand" to="/">Vue Router</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Contact Us</a>
+              <router-link class="nav-link" to="/contact">Contact Us</router-link>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Product
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">List</a></li>
-                <li><a class="dropdown-item" href="#">Details</a></li>
+                <li><router-link class="dropdown-item" to="/product/list">List</router-link></li>
+                <li><router-link class="dropdown-item" to="/product/details">Details</router-link></li>
               </ul>
             </li>
           </ul>
           <form class="d-flex">
-            <a class="nav-link" href="#">Login</a>
+            <router-link class="nav-link" to="/login">Login</router-link>
           </form>
         </div>
       </div>
