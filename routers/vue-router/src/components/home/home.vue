4cf7174cdf2a29479d7c1b0336791d8e6c071a86
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 03:02:39 +0500

    Navigation implemented from script to contactUs component.

--- routers/vue-router/src/components/home/home.vue
+++ routers/vue-router/src/components/home/home.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>Home Page</h1>
+    <button class="btn btn-primary" @click="GoToContactUs">Go To ContactUs</button>
 </template>
+
+
+<script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+    function GoToContactUs(){
+        router.push({ name: 'contactUs' });
+    }
+</script>
