89893b4bfba0feab11522416ad87cb67e5f5edc8
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 12:37:51 +0500

    App component implementation enhanced with axios and displayed destinations from json server.

--- api/src/components/App.vue
+++ api/src/components/App.vue
@@ -1,13 +1,34 @@
 <style scoped></style>
 
 <template>
-  <div>Learn Vue - API Request</div>
-
-  <hr>
+  <div>
+    <h1 class="text-primary text-center">Learn Vue - API Request</h1>
+  </div>
+  <!-- <hr>
   <div v-for="user in users" :key="user.id">
     <p>{{ user.name }}</p>
     <p>{{ user.email }}</p>
     <hr>
+  </div> -->
+  <div class="container p-4">
+    <div>
+      <h1 class="text-success text-center">TravelOPedia</h1>
+    </div>
+    <hr>
+    <table class="table table-striped table-light">
+      <thead>
+        <td>Name</td>
+        <td>Days</td>
+        <td>Price</td>
+      </thead>
+      <tbody class="table-light">
+        <tr v-for="destination in destinations" :key="destination.id">
+          <td>{{ destination.name }}</td>
+          <td>{{ destination.days }}</td>
+          <td>{{ destination.price }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -15,6 +36,7 @@
   import { reactive, onMounted } from 'vue';
   import axios from 'axios';
   let users = reactive([]);
+  let destinations = reactive([]);
 
   onMounted(() => {
     // axios.get('https://jsonplaceholder.typicode.com/users')
@@ -26,17 +48,26 @@
     //         console.log(error);
     //      });
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);  
-            users.push(...data);
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //      .then((response) => response.json())
+    //      .then((data) => {
+    //         console.log(data);  
+    //         users.push(...data);
+    //      }).catch((error) => {
+    //         console.log('Error Occured');  
+    //         console.log(error);
+    //      });
+
+    // console.log('users');
+    // console.log(users);
+
+    axios.get('http://localhost:3000/destination')
+         .then((response) => {
+            console.log(response);  
+            destinations.push(...response.data);
          }).catch((error) => {
             console.log('Error Occured');  
             console.log(error);
          });
-
-    console.log('users');
-    console.log(users);
   });
 </script>
