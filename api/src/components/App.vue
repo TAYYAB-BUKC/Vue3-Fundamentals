8bc698f90088312855c0241252309b19d278b165
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-15 13:10:13 +0500

    Loader implemented during API calls.

--- api/src/components/App.vue
+++ api/src/components/App.vue
@@ -1,44 +1,114 @@
-<style scoped></style>
+<style scoped>
+  .loader {
+    color: #ffffff;
+    font-size: 10px;
+    width: 1em;
+    height: 1em;
+    border-radius: 50%;
+    position: relative;
+    text-indent: -9999em;
+    animation: mulShdSpin 1.3s infinite linear;
+    transform: translateZ(0);
+  }
+
+  @keyframes mulShdSpin {
+    0%,
+    100% {
+      box-shadow: 0 -3em 0 0.2em, 
+      2em -2em 0 0em, 3em 0 0 -1em, 
+      2em 2em 0 -1em, 0 3em 0 -1em, 
+      -2em 2em 0 -1em, -3em 0 0 -1em, 
+      -2em -2em 0 0;
+    }
+    12.5% {
+      box-shadow: 0 -3em 0 0, 2em -2em 0 0.2em, 
+      3em 0 0 0, 2em 2em 0 -1em, 0 3em 0 -1em, 
+      -2em 2em 0 -1em, -3em 0 0 -1em, 
+      -2em -2em 0 -1em;
+    }
+    25% {
+      box-shadow: 0 -3em 0 -0.5em, 
+      2em -2em 0 0, 3em 0 0 0.2em, 
+      2em 2em 0 0, 0 3em 0 -1em, 
+      -2em 2em 0 -1em, -3em 0 0 -1em, 
+      -2em -2em 0 -1em;
+    }
+    37.5% {
+      box-shadow: 0 -3em 0 -1em, 2em -2em 0 -1em,
+      3em 0em 0 0, 2em 2em 0 0.2em, 0 3em 0 0em, 
+      -2em 2em 0 -1em, -3em 0em 0 -1em, -2em -2em 0 -1em;
+    }
+    50% {
+      box-shadow: 0 -3em 0 -1em, 2em -2em 0 -1em,
+      3em 0 0 -1em, 2em 2em 0 0em, 0 3em 0 0.2em, 
+      -2em 2em 0 0, -3em 0em 0 -1em, -2em -2em 0 -1em;
+    }
+    62.5% {
+      box-shadow: 0 -3em 0 -1em, 2em -2em 0 -1em,
+      3em 0 0 -1em, 2em 2em 0 -1em, 0 3em 0 0, 
+      -2em 2em 0 0.2em, -3em 0 0 0, -2em -2em 0 -1em;
+    }
+    75% {
+      box-shadow: 0em -3em 0 -1em, 2em -2em 0 -1em, 
+      3em 0em 0 -1em, 2em 2em 0 -1em, 0 3em 0 -1em, 
+      -2em 2em 0 0, -3em 0em 0 0.2em, -2em -2em 0 0;
+    }
+    87.5% {
+      box-shadow: 0em -3em 0 0, 2em -2em 0 -1em, 
+      3em 0 0 -1em, 2em 2em 0 -1em, 0 3em 0 -1em, 
+      -2em 2em 0 0, -3em 0em 0 0, -2em -2em 0 0.2em;
+    }
+  }
+</style>
 
 <template>
   <div>
-    <h1 class="text-primary text-center">Learn Vue - API Request</h1>
-  </div>
-  <div v-if="users.length > 0">
-    <hr>
-    <div v-for="user in users" :key="user.id">
-      <p>{{ user.name }}</p>
-      <p>{{ user.email }}</p>
-      <hr>
-    </div>
-  </div>
-  <div class="container p-4">
     <div>
-      <h1 class="text-success text-center">TravelOPedia</h1>
+      <h1 class="text-primary text-center">Learn Vue - API Request</h1>
     </div>
-    <hr>
-    <table class="table table-striped table-light">
-      <thead>
-        <td>Name</td>
-        <td>Days</td>
-        <td>Price</td>
-      </thead>
-      <tbody class="table-light">
-        <tr v-for="destination in destinations" :key="destination.id">
-          <td>{{ destination.name }}</td>
-          <td>{{ destination.days }}</td>
-          <td>{{ destination.price }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="mt-2 text-center" v-if="isLoading" style="padding: 50px;">
+      <span class="loader"></span>
+    </div>
+    <!-- <div class="mt-2 text-center">
+      <span class="loader"></span>
+    </div> -->
+    <div v-if="users.length > 0">
+      <hr>
+      <div v-for="user in users" :key="user.id">
+        <p>{{ user.name }}</p>
+        <p>{{ user.email }}</p>
+        <hr>
+      </div>
+    </div>
+    <div v-if="destinations.length > 0" class="container p-4 bg-white">
+      <div>
+        <h1 class="text-success text-center">TravelOPedia</h1>
+      </div>
+      <hr>
+      <table class="table table-striped table-light">
+        <thead>
+          <td>Name</td>
+          <td>Days</td>
+          <td>Price</td>
+        </thead>
+        <tbody class="table-light">
+          <tr v-for="destination in destinations" :key="destination.id">
+            <td>{{ destination.name }}</td>
+            <td>{{ destination.days }}</td>
+            <td>{{ destination.price }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script setup>
-  import { reactive, onMounted } from 'vue';
+  import { reactive, onMounted, ref } from 'vue';
   import axios from 'axios';
   let users = reactive([]);
   let destinations = reactive([]);
+  let isLoading= ref(true);
 
   onMounted(() => {
     //LoadUsersWithAxios();
@@ -75,13 +145,18 @@
   }
 
   function LoadDestinationsWithAxios(){
+    isLoading.value = true;
     axios.get('http://localhost:3000/destination')
          .then((response) => {
-            console.log(response);  
-            destinations.push(...response.data);
+            new Promise((resolve) => setTimeout(resolve, 5000)).then(() => {
+              destinations.push(...response.data);
+              console.log(response);  
+              isLoading.value = false;
+            });
          }).catch((error) => {
             console.log('Error Occured');  
             console.log(error);
+            isLoading.value = false;
          });
   }
 </script>
