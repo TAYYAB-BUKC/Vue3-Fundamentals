bfb6d4f5a4fe1838e8300639a919ed22a8a68369
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-16 20:38:25 +0500

    Display score and max score from gamestore in Home component implemented.

--- pinia/vue-pinia/src/components/Home.vue
+++ pinia/vue-pinia/src/components/Home.vue
@@ -7,8 +7,8 @@
         <h2 class="text-success">LET'S PLAY THE GAME</h2>
 
         <div>
-            <h3 class="text-primary pb-3">Current Score: </h3>
-            <h3 class="text-primary pb-3">Max Score: </h3>
+            <h3 class="text-primary pb-3">Current Score: {{ gameStore.score }}</h3>
+            <h3 class="text-primary pb-3">Max Score: {{ gameStore.maxHealth }}</h3>
         </div>
         <br>
         <div class="row" style="display: flex;justify-content: center;">
@@ -26,4 +26,6 @@
 </template>
 
 <script setup>
+    import { useGameStore } from '@/stores/gameStores';
+    const gameStore = useGameStore();
 </script>
