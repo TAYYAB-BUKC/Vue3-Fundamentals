9da68e48b42048795f308a264380c3129bb0997a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-16 20:00:21 +0500

    Routes added in router configuration.

--- pinia/vue-pinia/src/router/index.js
+++ pinia/vue-pinia/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/Home.vue'
+import Info from '@/components/Info.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {  name: 'home', path: '/', component: Home },
+    {  name: 'advancehome', redirect: { name: 'home' } },
+    {  name: 'info', path: '/info', component: Info }    
+  ],
 })
 
 export default router
