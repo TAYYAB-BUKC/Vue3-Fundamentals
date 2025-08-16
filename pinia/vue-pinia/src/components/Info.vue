b9e5bdc785604faa5d574b129355ab5a920f66d0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-16 21:57:22 +0500

    Info component template implemented.

--- pinia/vue-pinia/src/components/Info.vue
+++ pinia/vue-pinia/src/components/Info.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Info page</h1>
+    <div class="text-center">
+        <h1 class="text-success">This is the Info page</h1>
+        <p>
+            This is a game where you can increment, decrement or randomize the score.
+            The score starts at 0and you can win by reaching {{ gameStore.maxHealth }}.
+        </p>
+        <p>Winning Score: </p>
+        <input v-model="gameStore.maxHealth" />    
+    </div>
 </template>
+
+<script setup>
+    import { useGameStore } from '@/stores/gameStores';
+    const gameStore = useGameStore();
+</script>
