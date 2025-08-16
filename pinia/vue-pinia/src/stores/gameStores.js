0a2de1c1249673304c1d647f7052a22b1cca6a73
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-16 20:49:50 +0500

    Getters implemented in GramStore.

--- pinia/vue-pinia/src/stores/gameStores.js
+++ pinia/vue-pinia/src/stores/gameStores.js
@@ -7,4 +7,19 @@ export const useGameStore = defineStore('GameStore', {
         maxAttack: 30,
         maxDefense: 10 
     }),
+    getters:{
+        getScore() {
+            return this.score;
+        },
+        getNextAttack() {
+            let attack = Math.floor(Math.random() * this.maxAttack) + 1;
+            console.log('NextAttack: ' + attack);
+            return attack;
+        },
+        getNextDefense() {
+            let defense = Math.floor(Math.random() * this.maxDefense) + 1;
+            console.log('NextDefense: ' + defense);
+            return defense;
+        }
+    }
 })
