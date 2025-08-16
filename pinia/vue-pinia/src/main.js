a467b97d589c9d5d9ec6f5669b4ed5214aeb32d3
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-16 20:10:40 +0500

    Pinia support added.

--- pinia/vue-pinia/src/main.js
+++ pinia/vue-pinia/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
-
+import { createPinia } from 'pinia';
 import App from './components/App.vue'
 import router from './router'
 
-const app = createApp(App)
 
-app.use(router)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
