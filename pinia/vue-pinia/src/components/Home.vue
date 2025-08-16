b705bb6dda3d087c9ffe51ecaf4cd3eb6676aeb2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-16 20:34:15 +0500

    Home component template updated.

--- pinia/vue-pinia/src/components/Home.vue
+++ pinia/vue-pinia/src/components/Home.vue
@@ -1,3 +1,29 @@
+<style scoped>
+</style>
+
 <template>
-    <h1>Home page</h1>
+    <div class="text-center">
+        <h1 class="text-success">ARE YOU READY!!!!</h1>
+        <h2 class="text-success">LET'S PLAY THE GAME</h2>
+
+        <div>
+            <h3 class="text-primary pb-3">Current Score: </h3>
+            <h3 class="text-primary pb-3">Max Score: </h3>
+        </div>
+        <br>
+        <div class="row" style="display: flex;justify-content: center;">
+            <div class="col-5 m-5">
+                <button class="form-control btn btn-success p-4">Increment</button>
+            </div>
+            <div class="col-5 m-5">
+                <button class="form-control btn btn-danger p-4">Decrement</button>
+            </div>
+            <div class="col-6 pt-4" >
+                <button class="form-control btn btn-primary p-4">Random</button>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+</script>
